c821cb42c27f53d314e5de2fd33c86b518459bd1
joshuamarango <josh@beatsfront.com>
2023-06-05 17:06:27 +0100

    adding ability to query one field

--- src/core/repository/dynamodb.repository.ts
+++ src/core/repository/dynamodb.repository.ts
@@ -1,5 +1,5 @@
 import 'reflect-metadata';
-import { DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb';
+import { DynamoDBDocumentClient, PutCommand, QueryCommand } from '@aws-sdk/lib-dynamodb';
 import { DynamoDBService } from '../dynamodb.service';
 
 type ExtractProps<T> = {
@@ -37,5 +37,28 @@ export class DynamoDBRepository<T> {
 
     await this.ddbDocClient.send(command);
     return item;
+  }
+
+  async queryOne(keys: { [index: string]: any }, indexName?: string): Promise<T> {
+    let keyNames = Object.keys(keys);
+    let command = new QueryCommand({
+      TableName: this.getTableName(),
+      KeyConditionExpression: `#${keyNames[0]} = :${keyNames[0]} AND #${keyNames[1]} = :${keyNames[1]}`,
+      ExpressionAttributeNames: {},
+      ExpressionAttributeValues: {},
+    });
+
+    command.input.ExpressionAttributeNames![`#${keyNames[0]}`] = keyNames[0];
+    command.input.ExpressionAttributeNames![`#${keyNames[1]}`] = keyNames[1];
+
+    command.input.ExpressionAttributeValues![`:${keyNames[0]}`] = keys[keyNames[0]];
+    command.input.ExpressionAttributeValues![`:${keyNames[1]}`] = keys[keyNames[1]];
+
+    if (indexName) {
+      command.input.IndexName = indexName;
+    }
+
+    const item = await this.ddbDocClient.send(command);
+    return item.Items ? item.Items[0] as T : {} as T;
   }
 }
